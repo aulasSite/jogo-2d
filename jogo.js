2be69a715e1eb627906bd0a4d0993ee10617e9d4
unknown <tito.guidotti@gmail.com>
2017-10-22 17:34:33 -0200

    muda o jeito do personagem mover, inicia mult. de zumbis

--- jogo.js
+++ jogo.js
@@ -1,88 +1,123 @@
 $(function () {
 	var player='<div id="personagem"></div>';
-	var divZumbi='<div id="zumbi1" class="zumbi"></div>';
+	var numeroDeZumbi = 0;
+	var divZumbi='<div id="zumbi0" class="zumbi"></div>';
 	$('#map').append(player);
 	$('#map').append(divZumbi);
-
+	moveZumbi('#zumbi0');
+	
+	setInterval(function(){
+		numeroDeZumbi++;
+		divZumbi='<div id="zumbi' + numeroDeZumbi + '" class="zumbi"></div>';
+		$('#map').append(divZumbi);
+		var nome = "'#zumbi" + numeroDeZumbi + "'";
+		moveZumbi(nome);
+	},6000);
 	
 
-function moveJogador() {
-    $('html').keydown(function(e){
-       var direction = e.which;
-	   $("#personagem").stop();
-	   var esquerda = $('#personagem').css('left');
-		esquerda = esquerda.replace('px','');
-	   var baixo = $('#personagem').css('bottom');
-	   baixo = baixo.replace('px','');
-	   if (direction == 37 && esquerda > 0) {
-            $("#personagem").animate({left:  "-=2%"}, 2 , function(){});
-        }
-        if (direction == 38 && baixo < 450) {	
-             $("#personagem").animate({bottom:  "+=2%"}, 2 , function(){});
-        }
-        if (direction == 39 && esquerda < 760) {
-             $("#personagem").animate({left:  "+=2%"}, 2 , function(){});
-        }
-        if (direction == 40 && baixo > 0) {
-             $("#personagem").animate({bottom:  "-=2%"}, 2 , function(){});
-        }
-		direction = 0;
-    }); 
-}
-	
-	
-	
-	
-	
-	
-function moveZumbi(adress) {
-	console.log(adress);
-	   adress.stop();
-	   var esquerda = $('#personagem').css('left');
-		esquerda = esquerda.replace('px','');
-	   var baixo = $('#personagem').css('bottom');
-	   baixo = baixo.replace('px','');
-	   var zumbiEsquerda = adress.css('left');
-	   zumbiEsquerda = zumbiEsquerda.replace( 'px' , '' );
-	   var zumbiBaixo = adress.css('bottom');
-	   zumbiBaixo = zumbiBaixo.replace( 'px' , '' );
-	   console.log(zumbiBaixo);
-	   console.log(baixo);
-	   $("#p").append("text<br/>");
-	   if(zumbiBaixo < baixo){
-		   adress.animate({bottom:  "+=2%"}, 2 , function(){});
-	   }
-}	
-	
-	
-	
-	
-	setInterval(function(){ 
-	   var adress=$('#zumbi1');
-	   adress.stop();
-	   var esquerda = $('#personagem').css('left');
-		esquerda = esquerda.replace('px','');
-	   var baixo = $('#personagem').css('bottom');
-	   baixo = baixo.replace('px','');
-	   var zumbiEsquerda = adress.css('left');
-	   zumbiEsquerda = zumbiEsquerda.replace( 'px' , '' );
-	   var zumbiBaixo = adress.css('bottom');
-	   zumbiBaixo = zumbiBaixo.replace( 'px' , '' );
-	    console.log(zumbiBaixo);
-	    //console.log(baixo);
-	   // $("#p").append("text<br/>");
-	   console.log("if "+zumbiBaixo + "<" + baixo);
-	   if(zumbiBaixo < baixo){
-		   console.log("subinu");
-		   adress.animate({bottom:  "+=2%"}, 0 , function(){});
-	   } }, 200);
+	console.log("estoy aqui");
+	var pressedKeys = [];
+	var teclasEstado = [];
+	$(document.body).keydown(function (evt) {										//detecta quando abaixado
+		var li = pressedKeys[evt.keyCode];
+		if (!li) {
+				pressedKeys[evt.keyCode] = li;
+			}
+		if(jQuery.inArray( (evt.keyCode + "down"), teclasEstado) == -1){
+			//console.log(jQuery.inArray( (evt.keyCode + "down"), teclasEstado));
+				//console.log(evt.keyCode + "down");
+				var index = teclasEstado.indexOf(evt.keyCode + "up");
+				if (index !== -1) {
+					teclasEstado[index] = evt.keyCode + "down" ;
+				}
+				else{
+					teclasEstado.push(evt.keyCode + "down");
+				}
+			
+					console.log(teclasEstado);
+					
+					//console.log("tá na posição " + index);
+				}
+				
+			});
+			
+			$(document.body).keyup(function (evt) {								//detecta quando levanta
+				var li = pressedKeys[evt.keyCode];
+				if (!li) {
+				}
+				var index = teclasEstado.indexOf(evt.keyCode + "down");
+				if (index !== -1) {
+					teclasEstado[index] = evt.keyCode + "up" ;
+				}
+				else{
+					teclasEstado.push(evt.keyCode + "up");
+				}
+				
+				console.log(teclasEstado);
+				
+				
+				//console.log(evt.keyCode + "up");
+			});
 
 	
-	setInterval(moveJogador(), 100);
-	/* setInterval(
-		function(){
-		console.log("ola");
-		}, 100);*/
+	setInterval(function(){
+		var esquerda = $('#personagem').css('left');
+		esquerda = esquerda.replace('px','');
+		var baixo = $('#personagem').css('bottom');
+		baixo = baixo.replace('px','');
+		console.log(jQuery.inArray("37down" , teclasEstado));
+		 if (jQuery.inArray("37down" , teclasEstado) != -1 && esquerda > 0) {
+				$("#personagem").animate({left:  "-=2%"}, 2 , function(){});
+			}
+		if (jQuery.inArray("38down" , teclasEstado) != -1 && baixo < 450) {	
+			 $("#personagem").animate({bottom:  "+=2%"}, 2 , function(){});
+			 console.log("hello word");
+		}
+		if (jQuery.inArray("39down" , teclasEstado) != -1  && esquerda < 753) {
+				 $("#personagem").animate({left:  "+=2%"}, 2 , function(){});
+			}
+		if (jQuery.inArray("40down" , teclasEstado) != -1  && baixo > 0) {
+			 $("#personagem").animate({bottom:  "-=2%"}, 2 , function(){});
+		}
+	}, 60);
+	
+	
+		
+	
+	
+	
+	function moveZumbi(adr){
+		 var adress=$( adr );
+		setInterval(function(){ 
+		   adress.stop();
+		   
+		   var esquerda = parseInt( $('#personagem').css('left') );
+		   var baixo =       parseInt( $('#personagem').css('bottom') );
+		   var zumbiEsquerda = parseInt( adress.css('left') );
+		   var zumbiBaixo =       parseInt( adress.css('bottom') );
+		   
+		   //console.log("if "+zumbiBaixo + "<" + baixo);
+		   if ( zumbiBaixo < baixo ){
+			   //console.log("subinu");
+			   adress.animate({bottom:  "+=2%"}, 2 , function(){});
+		   } 
+		   else if ( zumbiBaixo > baixo ){
+			   //console.log("descenu");
+			   adress.animate({bottom:  "-=2%"}, 2 , function(){});
+		   } 
+		   
+			if ( zumbiEsquerda < esquerda ){
+			   //console.log("arduinu");
+			   adress.animate({left:  "+=2%"}, 2 , function(){});
+		   }
+			else if ( zumbiEsquerda > esquerda ){
+			   //console.log("arduvinu");
+			   adress.animate({left:  "-=2%"}, 2 , function(){});
+		   } 
+		   
+		}, 200);
+	}
+	
 	
 
 });
